5cf466a2fc6378d20a67e7b546e56397795ff384
IamStanStill <tom_osborne@xtrac.com>
2020-11-30 11:09:25 +0000

    added JS

---
+++ JS/HardnessConversionTool/hardnessconversion.js
@@ -0,0 +1,18 @@
+// Rockwell > Vickers 
+// >> y = 0.000111x^4 - 0.014722x^3 + 0.845702x^2 - 15.166443x + 303.499402
+
+// Vickers > Rockwell
+// >> y = 1E-07x^3 - 0.0003x^2 + 0.2748x - 29.619
+
+// Brinell > Vickers
+// >> y = 1.0732x - 5.0422
+
+// Vickers > Brinell
+// >> y = 0.9317x + 4.7088
+
+// Vickers > Approx. Tensile Strength
+// >> y = 0.2936x + 14.982
+
+// Brinell > Approx. Tensile Strength
+// >> y = 0.2736x + 18.668
+
