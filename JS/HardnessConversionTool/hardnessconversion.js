7bacaaac0b1d61ce94fba4fb89fbdc46ddf0a88e
IamStanStill <tom_osborne@xtrac.com>
2020-12-01 17:55:42 +0000

    v1.3 tidy

--- JS/HardnessConversionTool/hardnessconversion.js
+++ JS/HardnessConversionTool/hardnessconversion.js
@@ -1,20 +1,7 @@
-// Rockwell > Vickers 
-// >> y = 0.000111x^4 - 0.014722x^3 + 0.845702x^2 - 15.166443x + 303.499402
-
-// Vickers > Rockwell
-// >> y = 1E-07x^3 - 0.0003x^2 + 0.2748x - 29.619
-
-// Brinell > Vickers
-// >> y = 1.0732x - 5.0422
-
-// Vickers > Brinell
-// >> y = 0.9317x + 4.7088
-
-// Vickers > Approx. Tensile Strength
-// >> y = 0.2936x + 14.982
-
-// Brinell > Approx. Tensile Strength
-// >> y = 0.2736x + 18.668
+// Hardness Conversion Tool
+// Version: 1.3
+// Author: Tom Osborne
+// Date: 01/12/2020
 
 function vickersFromRockwell(Rockwell, ElementID) {
     let x_rw = Rockwell;
@@ -29,8 +16,6 @@ function vickersFromRockwell(Rockwell, ElementID) {
 
     y_vk = Math.round((y_vk + Number.EPSILON) * 1 ) / 1; //0 DP
 
-    //return y_vk;
-    console.log("Function vickersFromRockwell was run.");
     document.getElementById(elementID).innerHTML = y_vk;
 }
 
@@ -73,16 +58,3 @@ function vickersFromBrinell(Brinell, ElementID) {
 
     document.getElementById(elementID).innerHTML = y_vk;
 }
-
-const queryString = window.location.search;
-console.log("Query String = " + queryString);
-
-const urlParams = new URLSearchParams(queryString);
-
-const input_rw = urlParams.get('rockwell_ui');
-const input_vk = urlParams.get('vickers_ui');
-const input_br = urlParams.get('brinell_ui');
-
-console.log("Rockwell Input = " + input_rw);
-console.log("Vickers Input = " + input_vk);
-console.log("Brinell Input = " + input_br);
