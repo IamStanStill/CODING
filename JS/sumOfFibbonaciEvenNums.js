307f463e0b51ea6533f99bd03f9fdc0753824792
Stan <49242843+IamStanStill@users.noreply.github.com>
2020-12-08 17:11:49 +0000

    Update sumOfFibbonaciEvenNums.js
    Commented code

--- JS/sumOfFibbonaciEvenNums.js
+++ JS/sumOfFibbonaciEvenNums.js
@@ -6,22 +6,34 @@
 // See ref: - https://en.wikipedia.org/wiki/Fibonacci_number
 
 function fib() {
-  const maxNum = 4000000;
-  const f0 = 1;
-  const f1 = 2;
-  let n = 2; 
-  let k = 1;
-  let sum = 0;
-  let fibbArr = [f0, f1];
+  const maxNum = 4000000;   // Limit of sequence
+  const f0 = 1;             // index 0 value
+  const f1 = 2;             // index 1 value
+  let n = 0;                // fibbonaci number
+  let k = 1;                // Array index
+  let sum = 0;              // sum value
+  let fibbArr = [f0, f1];   // Initialise first two values of array
   
+  // Loop until fibbonaci number is above defined limit
   while (n <= maxNum) {
+    
+    // Calculate the next fibbonaci number, n, using the index k to define the current and previous numbers
     n = fibbArr[k] + fibbArr[k - 1];
+    
+    // Add fibbonaci number to the array at the end
     fibbArr.push(n);
+    
+    // Check if the current number is within the limit and if it is even
     if(n < maxNum && n % 2 === 0) {
+      // If True, add the number to the existing sum value
       sum = sum + n;
     }    
+    
+    // Increment index
     k += 1;
   }
+  
+  // Return the sum value
   return sum;
 }
 fib();
