a987011add2f808333d5ce3109c60d8423c9518d
Stan <49242843+IamStanStill@users.noreply.github.com>
2020-12-08 10:30:39 +0000

    Update sumOfFibbonaciEvenNums.js
    Tidy

--- JS/sumOfFibbonaciEvenNums.js
+++ JS/sumOfFibbonaciEvenNums.js
@@ -1,7 +1,8 @@
-// https://projecteuler.net/problem=2
+// Problem: - https://projecteuler.net/problem=2
 // By considering the terms in the Fibonacci sequence whose
 // values do not exceed four million, find the sum of the 
 // even-valued terms.
+// See ref: - https://en.wikipedia.org/wiki/Fibonacci_number
 
 function fib() {
   const maxNum = 4000000;
@@ -10,12 +11,10 @@ function fib() {
   let n = 2;
   let k = 1;
   let sum = 0;
-
   let fibbArr = [f0, f1];
   let fibbArrEven = [];
   
   // Create Fibbonaci Array
-  // https://en.wikipedia.org/wiki/Fibonacci_number
   while (n <= maxNum) {
     n = fibbArr[k] + fibbArr[k - 1];
     fibbArr.push(n)
@@ -32,13 +31,8 @@ function fib() {
   // Find the sum of the items in the array
   for (let i = 0; i < fibbArrEven.length; i++) {
   	sum = sum + fibbArrEven[i];
-  }
-  
-  for (let i = 0; i < fibbArrEven.length; i++) {
-    console.log(fibbArrEven[i]);
-  }
-  
-  console.log("Sum = " + sum);
+  }  
+  return sum;
 }
 
 fib();
